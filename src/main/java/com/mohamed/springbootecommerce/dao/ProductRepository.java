9bf053f1704274984986db9526a6db2e0762cc64
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-13 09:25:25 +0200

    create product repository interface

---
+++ src/main/java/com/mohamed/springbootecommerce/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.mohamed.springbootecommerce.dao;
+
+import com.mohamed.springbootecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
