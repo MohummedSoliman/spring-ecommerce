f970a10073e696173a75c8f42f311e14629574c5
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-14 22:15:35 +0200

    enable cross origin

--- src/main/java/com/mohamed/springbootecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/mohamed/springbootecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.mohamed.springbootecommerce.dao;
 import com.mohamed.springbootecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
