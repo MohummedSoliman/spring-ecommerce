1e16da36aecf8fd39bdfb2454eb16b57501989f8
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-20 07:38:40 +0200

    create service interface and service implementation

--- src/main/java/com/mohamed/springbootecommerce/service/CheckoutService.java
+++ src/main/java/com/mohamed/springbootecommerce/service/CheckoutService.java
@@ -1,4 +1,9 @@
 package com.mohamed.springbootecommerce.service;
 
+import com.mohamed.springbootecommerce.dto.Purchase;
+import com.mohamed.springbootecommerce.dto.PurchaseResponse;
+
 public interface CheckoutService {
+
+    PurchaseResponse placeOrder(Purchase purchase);
 }
--- src/main/java/com/mohamed/springbootecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/mohamed/springbootecommerce/service/CheckoutServiceImpl.java
@@ -1,4 +1,22 @@
 package com.mohamed.springbootecommerce.service;
 
-public class CheckoutServiceImpl {
+import com.mohamed.springbootecommerce.dao.CustomerRepository;
+import com.mohamed.springbootecommerce.dto.Purchase;
+import com.mohamed.springbootecommerce.dto.PurchaseResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService{
+
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public CheckoutServiceImpl(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+    @Override
+    public PurchaseResponse placeOrder(Purchase purchase) {
+        return null;
+    }
 }
